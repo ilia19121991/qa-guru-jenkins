fd89aaf30d26a2367f39d22a34043b67d0edafb7
Ilia <ilia19011991@gmail.com>
2022-12-28 20:15:16 +0300

    added attachments

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -1,17 +1,13 @@
-package demoqa;
+package tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.URI;
-
-import static io.qameta.allure.internal.shadowed.jackson.databind.type.LogicalType.Map;
 
 
 public class TestBase {
@@ -21,8 +17,13 @@ public class TestBase {
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-
+        // Configuration.timeout = 10000;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
 
     }
 
@@ -34,9 +35,10 @@ public class TestBase {
 
     @AfterEach
     void addAttachments(){
-        // screenshot
-        // pagesource
-        // console logs
-        // video
+
+        Attach.screenshotAs("Last Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
