40b7112f0551070241b19ccd98213508ba4e25b7
Ilia <ilia19011991@gmail.com>
2022-12-28 19:10:59 +0300

    added remote url (Selenoid)

---
+++ src/test/java/demoqa/PracticeFormTests.java
@@ -0,0 +1,70 @@
+package demoqa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+
+public class PracticeFormTests extends TestBase {
+
+
+    @Test
+    void practiceFormTest() {
+
+        String firstName = "Anna";
+        String lastName = "Petrova";
+        String email = "anna@gmail.com";
+        String mobile = "9166789010";
+        String subject1 = "Chemistry";
+        String subject2 = "Maths";
+        String currentAddress = "Moscow";
+        String submitText = "Thanks for submitting the form";
+
+        step("Open registration form", () -> {
+            open("/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            executeJavaScript("$('footer').remove()");
+            executeJavaScript("$('#fixedban').remove()");
+        });
+        step("Fill form", () -> {
+            $("#firstName").setValue(firstName);
+            $("#lastName").setValue(lastName);
+            $("#userEmail").setValue(email);
+            $("#genterWrapper").$(byText("Female")).click();
+            $("#userNumber").setValue(mobile);
+
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("December");
+            $(".react-datepicker__year-select").selectOption("1991");
+            $(".react-datepicker__day--005:not(.react-datepicker__day--outside-month)").click();
+
+            $("#subjectsInput").setValue(subject1).pressEnter().setValue(subject2).pressEnter();
+
+            $("#hobbiesWrapper").$(byText("Sports")).click();
+            $("#hobbiesWrapper").$(byText("Music")).click();
+
+            $("#uploadPicture").uploadFromClasspath("searchElements.jpg");
+
+            $("#currentAddress").setValue(currentAddress);
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText("NCR")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Gurgaon")).click();
+
+            $("#submit").click();
+        });
+        step("Check form results", () -> {
+            $(".modal-dialog").should(appear);
+            $(".modal-header").shouldHave(text(submitText));
+
+            $(".modal-body").shouldHave(text(firstName), text(lastName), text(email), text("Female"), text(mobile),
+                text("05 December,1991"), text(subject1), text(subject2), text("Sports, Music"), text("searchElements.jpg"), text(currentAddress), text("NCR Gurgaon"));
+            $(".modal-body").shouldNotHave(text("Reading"));
+        });
+
+    }
+}
---
+++ src/test/java/demoqa/TestBase.java
@@ -0,0 +1,41 @@
+package demoqa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URI;
+
+import static io.qameta.allure.internal.shadowed.jackson.databind.type.LogicalType.Map;
+
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+    }
+
+    @BeforeEach
+    void addListener(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments(){
+        // screenshot
+        // pagesource
+        // console logs
+        // video
+    }
+}
