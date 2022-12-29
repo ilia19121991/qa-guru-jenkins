9326ae9cdd2e2a3db42f0d388e8875a178c511a1
Ilia <ilia19011991@gmail.com>
2022-12-29 17:35:32 +0300

    added "," to system properties tests

--- src/test/java/tests/properties/SystemPropertiesTests.java
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -29,7 +29,7 @@ public class SystemPropertiesTests {
     @Test
     void simpleProperty3Test() {
         System.setProperty("browser", "opera"); // установили значение браузера "opera"
-        String browserName = System.getProperty("browser", "firefox"); /*запросили значение браузера
+        String browserName = System.getProperty("browser", "firefox"); /* запросили значение браузера
         с указанием, что по дефолту стоял firefox*/
 
         System.out.println(browserName); // opera
@@ -54,7 +54,7 @@ public class SystemPropertiesTests {
     @Test
     @Tag("hello")
     void simpleProperty5Test(){
-        System.out.println("Hello ," + System.getProperty("user_name", "unknown student"));
+        System.out.println("Hello," + System.getProperty("user_name", "unknown student"));
 
         /*
         gradle clean hello_test
