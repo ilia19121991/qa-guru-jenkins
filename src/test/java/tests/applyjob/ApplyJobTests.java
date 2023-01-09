b575995e4e5e7a02c641af09eb558dfca8a1194d
Ilia <ilia19011991@gmail.com>
2023-01-09 17:05:59 +0300

    final version

--- src/test/java/tests/applyjob/ApplyJobTests.java
+++ src/test/java/tests/applyjob/ApplyJobTests.java
@@ -10,8 +10,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import tests.demoqa.TestBase;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
@@ -119,37 +118,6 @@ public class ApplyJobTests {
         step("Проверяем, что открылся раздел Сравнение", () -> {
             $(".top-compare__header").shouldHave(text("Часто сравниваемые"));
         });
-    }
-
-    @DisplayName("Поиск через кнопку Каталог на Главной странице (пока что фэйлится")
-    @Test
-    void searchByCatalogTest() {
-        Configuration.timeout = 15000;
-        Selenide.clearBrowserCookies();
-
-        step("Открываем сайт главную страницу DNS", () -> {
-            open("https://www.dns-shop.ru/");
-        });
-        step("Подтверждаем свой город", () -> {
-            $(".v-confirm-city__buttons_qv9").find(byText("Всё верно")).click();
-        });
-        step("Нажимаем на кнопку: Каталог", () -> {
-            $("[data-role=catalog-button]").click();
-        });
-        step("Наводим мышку на пункт: ПК, ноутбуки, периферия", () -> {
-            $(".header-menu-desktop-wrap").find(byText("ПК, ноутбуки, периферия")).click();
-        });
-        step("Выбираем пункт: Ноутбуки", () -> {
-            $(byXpath("//*[@id=\"header-search\"]/div/div[4]/div/div[2]/div/div[2]/div[1]/a[1]")).click();
-            //  $(".header-menu-desktop__second-level-wrap").find(byLinkText("noutbuki")).click();
-        });
-        step("Выбираем раздел Ноутбуки и аксессуары", () -> {
-            $(".subcategory__item-container").find(byText("Ноутбуки и аксессуары")).click();
-        });
-        step("Проверяем, что в первом элементе списка выдачи находится ноутбук", () -> {
-            $$("[data-id=product]").first().shouldHave(text("ноутбук"));
-        });
-
     }
 
 }
